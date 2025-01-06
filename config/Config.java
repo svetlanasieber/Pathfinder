6c3a4b52120c3c502341145e5144d93b9bd37f8d
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-07 00:38:22 +0100

    Create Config.java

---
+++ config/Config.java
@@ -0,0 +1,20 @@
+package bg.kukuruku.Pathfinder.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class Config {
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ModelMapper mapper() {
+        return new ModelMapper();
+    }
+}
