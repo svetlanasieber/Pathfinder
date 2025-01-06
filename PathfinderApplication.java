7f4ab525776c18f13f4716cf33e3e644afec0380
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-07 00:37:01 +0100

    Create PathfinderApplication.java

---
+++ PathfinderApplication.java
@@ -0,0 +1,13 @@
+package bg.kukuruku.Pathfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathfinderApplication.class, args);
+	}
+
+}
